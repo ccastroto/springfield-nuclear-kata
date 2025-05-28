baed8095aa885c25c9c1cef60f03288b86c43df6
ccastroto <carloscastroaldrover@gmail.com>
2025-05-28 13:57:05 +0200

    test(employee): crear casos test y ejecutarlos

---
+++ src/test/java/com/springfield/powerplant/model/EmployeeTest.java
@@ -0,0 +1,37 @@
+package com.springfield.powerplant.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EmployeeTest {
+
+    private Employee employee;
+
+    @Before
+    public void setUp() {
+        employee = new Employee("Homer Simpson", 1, Department.REACTOR_CONTROL,
+                ExperienceLevel.NOVATO, Shift.MORNING);
+    }
+
+    @Test
+    public void testEmployee() {
+        assertEquals( "Homer Simpson", employee.getName());
+    }
+    @Test
+    public void testSetID() {
+        assertEquals(1, employee.getId());
+    }
+    @Test
+    public void testDepartment() {
+        assertEquals(Department.REACTOR_CONTROL, employee.getDepartment(Department.REACTOR_CONTROL));
+    }
+    @Test
+    public void testExperienceLevel() {
+        assertEquals(ExperienceLevel.NOVATO, employee.getExperienceLevel(ExperienceLevel.NOVATO));
+    }
+    @Test
+    public void testShift() {
+        assertEquals(Shift.MORNING, employee.getShift(Shift.MORNING));
+    }
+}
