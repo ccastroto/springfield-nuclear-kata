9c904ac0403008ae92541934c578f50756564fcd
ccastroto <carloscastroaldrover@gmail.com>
2025-05-28 13:04:48 +0200

    feat(shift): definir el enum del tipo de turno

---
+++ src/main/java/com/springfield/powerplant/model/Shift.java
@@ -0,0 +1,15 @@
+package com.springfield.powerplant.model;
+
+public enum Shift {
+
+    MORNING, AFTERNOON, NIGHT;
+
+    private String shift;
+
+    Shift() {
+        this.shift = shift;
+    }
+    public String getShift() {
+        return this.shift;
+    }
+}
