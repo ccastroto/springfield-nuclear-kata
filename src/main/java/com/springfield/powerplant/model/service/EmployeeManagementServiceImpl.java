99c5893475f93842e1cf34077cbf940d37d460e7
ccastroto <carloscastroaldrover@gmail.com>
2025-05-28 12:32:55 +0200

    feat(employeemanagementserviceimpl): crear la clase donde gestiona los empleados

---
+++ src/main/java/com/springfield/powerplant/model/service/EmployeeManagementServiceImpl.java
@@ -0,0 +1,9 @@
+package com.springfield.powerplant.model.service;
+
+public class EmployeeManagementServiceImpl implements EmployeeManagementService {
+
+    private  EmployeeManagementServiceImpl  employeeservice;
+
+    public void EmployeeService() {}
+
+}
