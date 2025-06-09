3d990e33a2af1fcbad8d96a545423c009291f252
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-06-09 14:43:01 +0200

    test(employeemanagementserviceimpl): crear caso test de buscar empleado

--- src/test/java/com/springfield/powerplant/model/EmployeeManagementServiceTest.java
+++ src/test/java/com/springfield/powerplant/model/EmployeeManagementServiceTest.java
@@ -2,12 +2,14 @@ package com.springfield.powerplant.model;
 
 import com.springfield.powerplant.service.EmployeeManagementServiceImpl;
 import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class EmployeeManagementServiceTest {
 
     private EmployeeManagementServiceImpl employeeManagementService;
-
     @Test
     public void createEmployee() {
         Employee createEmployee = new Employee("Sr Burns", 5, Department.ADMINISTRATION,
@@ -18,5 +20,19 @@ public class EmployeeManagementServiceTest {
         assertEquals(ExperienceLevel.EXPERTO, createEmployee.getExperienceLevel());
         assertEquals(Shift.MORNING, createEmployee.getShift());
         assertEquals(Employee.class, createEmployee.getClass());
+    }
+    @Test
+    public void findEmployee() {
+        Employee empBurns = new Employee("Sr Burns", 5, Department.ADMINISTRATION,
+                ExperienceLevel.EXPERTO, Shift.MORNING);
+        Employee empCarl = new Employee("Carl Carlson", 3, Department.MAINTENANCE,
+                ExperienceLevel.INTERMEDIO, Shift.NIGHT);
+        List<Employee> employees = List.of(empBurns, empCarl);
+        employeeManagementService = new EmployeeManagementServiceImpl();
+        employeeManagementService.setEmployees(employees);
+        List<Employee> result = employeeManagementService.findEmployeesByDepartment(Department.ADMINISTRATION);
+
+        assertEquals(1, result.size());
+        assertTrue(result.contains(empBurns));
     }
 }
