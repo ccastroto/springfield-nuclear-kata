17006bfcd4a7363a89d8d82960425c16088c002c
ccastroto <carloscastroaldrover@gmail.com>
2025-06-11 17:29:17 +0200

    test(employeemanagementserviceimpl): definir caso test de cambio de exp y refactorizar el código

--- src/test/java/com/springfield/powerplant/model/EmployeeManagementServiceTest.java
+++ src/test/java/com/springfield/powerplant/model/EmployeeManagementServiceTest.java
@@ -1,6 +1,7 @@
 package com.springfield.powerplant.model;
 
 import com.springfield.powerplant.service.EmployeeManagementServiceImpl;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -10,29 +11,59 @@ import static org.junit.Assert.*;
 public class EmployeeManagementServiceTest {
 
     private EmployeeManagementServiceImpl employeeManagementService;
-    @Test
-    public void createEmployee() {
-        Employee createEmployee = new Employee("Sr Burns", 5, Department.ADMINISTRATION,
+    private Employee empCarl;
+    private Employee empBurns;
+
+    @Before
+    public void setUp() {
+        empBurns = new Employee("Sr Burns", 5, Department.ADMINISTRATION,
                 ExperienceLevel.EXPERTO, Shift.MORNING);
-        assertNotNull(createEmployee);
-        assertEquals("Sr Burns", createEmployee.getName());
-        assertEquals(Department.ADMINISTRATION, createEmployee.getDepartment());
-        assertEquals(ExperienceLevel.EXPERTO, createEmployee.getExperienceLevel());
-        assertEquals(Shift.MORNING, createEmployee.getShift());
-        assertEquals(Employee.class, createEmployee.getClass());
-    }
-    @Test
-    public void findEmployee() {
-        Employee empBurns = new Employee("Sr Burns", 5, Department.ADMINISTRATION,
-                ExperienceLevel.EXPERTO, Shift.MORNING);
-        Employee empCarl = new Employee("Carl Carlson", 3, Department.MAINTENANCE,
+        empCarl = new Employee("Carl Carlson", 3, Department.MAINTENANCE,
                 ExperienceLevel.INTERMEDIO, Shift.NIGHT);
         List<Employee> employees = List.of(empBurns, empCarl);
         employeeManagementService = new EmployeeManagementServiceImpl();
         employeeManagementService.setEmployees(employees);
+    }
+
+    @Test
+    public void createEmployeeTest() {
+        assertNotNull(empBurns);
+        assertEquals("Sr Burns", empBurns.getName());
+        assertEquals(Department.ADMINISTRATION, empBurns.getDepartment());
+        assertEquals(ExperienceLevel.EXPERTO, empBurns.getExperienceLevel());
+        assertEquals(Shift.MORNING, empBurns.getShift());
+        assertEquals(Employee.class, empBurns.getClass());
+    }
+    @Test
+    public void findEmployeeTest() {
         List<Employee> result = employeeManagementService.findEmployeesByDepartment(Department.ADMINISTRATION);
 
         assertEquals(1, result.size());
         assertTrue(result.contains(empBurns));
+    }
+    /* // caso test findEmployee sin Before
+    @Test
+    public void findEmployeTest(){
+    // Arange
+    Employee empBurns = new Employee("Sr Burns", 5, Department.ADMINISTRATION,
+           ExperienceLevel.EXPERTO, Shift.MORNING);
+    Employee empCarl = new Employee("Carl Carlson", 3, Department.MAINTENANCE,
+           ExperienceLevel.INTERMEDIO, Shift.NIGHT);
+    List<Employee> employees = List.of(empBurns, empCarl);
+    employeeManagementService = new EmployeeManagementServiceImpl()
+    employeeManagementService.setEmployees(employees);
+    // Act
+    List<Employee> result = employeeManagementService.findEmployeesByDepartment(Department.ADMINISTRATION);
+    //Assert
+    assertEquals(1, result.size());
+    assertTrue(result.contains(empBurns));
+    }
+    */
+    @Test
+    public void changeExperienceLevelTest() {
+        employeeManagementService.changeEmployeeExperienceLevel(empCarl, ExperienceLevel.EXPERTO);
+
+        assertEquals(ExperienceLevel.EXPERTO, empCarl.getExperienceLevel());
+        assertTrue(empCarl.getExperienceLevel() == ExperienceLevel.EXPERTO);
     }
 }
